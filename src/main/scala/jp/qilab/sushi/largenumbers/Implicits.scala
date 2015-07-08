023470ce0c8e05f0564158e1ddd3394d012c99b8
qilab gamma <qilab@gmx.com>
2015-07-09 02:33:23 +0900

    RichInt

--- src/main/scala/jp/qilab/sushi/largenumbers/Implicits.scala
+++ src/main/scala/jp/qilab/sushi/largenumbers/Implicits.scala
@@ -1,9 +1,11 @@
 package jp.qilab.sushi.largenumbers
 
-import jp.qilab.sushi.runtime.RichBigInt
+import jp.qilab.sushi.runtime.{RichBigInt, RichInt}
 import scala.language.implicitConversions
 
 object Implicits {
+
+  implicit def intWrapper(x: Int) = new RichInt(x)
 
   implicit def bigIntWrapper(x: BigInt) = new RichBigInt(x)
 
---
+++ src/main/scala/jp/qilab/sushi/runtime/RichInt.scala
@@ -0,0 +1,21 @@
+package jp.qilab.sushi.runtime
+
+import jp.qilab.sushi.largenumbers._
+import scala.annotation.tailrec
+
+final class RichInt(val self: Int) {
+
+  def pow(exp: Int): BigInt = BigInt(self).pow(exp)
+  def pow(exp: BigInt): BigInt = jp.qilab.sushi.largenumbers.pow(self, exp)
+
+  def !(): BigInt = factorialRec(1, self)
+
+  @tailrec
+  private[this] def factorialRec(c: BigInt, n: Int): BigInt = {
+    require(n >= 0)
+    if (n == 0) c else factorialRec(c * n, n - 1)
+  }
+
+  def ^^(exp: Int): BigInt = tetration(self, exp)
+
+}
