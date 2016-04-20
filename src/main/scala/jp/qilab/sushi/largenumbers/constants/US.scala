94d5d82c3b259f3772993d9965e4debadbd1c2ac
qilab- <qilab@gmx.com>
2016-04-21 00:29:12 +0900

    names of large numbers in US

---
+++ src/main/scala/jp/qilab/sushi/largenumbers/constants/US.scala
@@ -0,0 +1,85 @@
+package jp.qilab.sushi.largenumbers.constants
+
+import jp.qilab.sushi.largenumbers._
+import jp.qilab.sushi.largenumbers.Implicits._
+
+object US {
+
+  def One: Int = 1
+  def Ten: Int = 10
+  def Hundred: Int = 100
+  def Thousand: Int = 10.pow(3).toInt
+  def Million: Int = 10.pow(6).toInt
+  def Billion: Int = 10.pow(9).toInt
+  def Trillion: Long = 10.pow(12).toLong
+  def Quadrillion: Long = 10.pow(15).toLong
+  def Quintillion: Long = 10.pow(18).toLong
+  def Sextillion: BigInt = 10.pow(21)
+  def Septillion: BigInt = 10.pow(24)
+  def Octillion: BigInt = 10.pow(27)
+  def Nonillion: BigInt = 10.pow(30)
+  def Decillion: BigInt = 10.pow(33)
+  def Undecillion: BigInt = 10.pow(36)
+  def Duodecillion: BigInt = 10.pow(39)
+  def Tredecillion: BigInt = 10.pow(42)
+  def Quattuordecillion: BigInt = 10.pow(45)
+  def Quindecillion: BigInt = 10.pow(48)
+  def Sexdecillion: BigInt = 10.pow(51)
+  def Septendecillion: BigInt = 10.pow(54)
+  def Octodecillion: BigInt = 10.pow(57)
+  def Novemdecillion: BigInt = 10.pow(60)
+  def Vigintillion: BigInt = 10.pow(63)
+  def Unvigintillion: BigInt = 10.pow(66)
+  def Duovigintillion: BigInt = 10.pow(69)
+  def Tresvigintillion: BigInt = 10.pow(72)
+  def Quattuorvigintillion: BigInt = 10.pow(75)
+  def Quinquavigintillion: BigInt = 10.pow(78)
+  def Sesvigintillion: BigInt = 10.pow(81)
+  def Septemvigintillion: BigInt = 10.pow(84)
+  def Octovigintillion: BigInt = 10.pow(87)
+  def Novemvigintillion: BigInt = 10.pow(90)
+  def Trigintillion: BigInt = 10.pow(93)
+  def Untrigintillion: BigInt = 10.pow(96)
+  def Duotrigintillion: BigInt = 10.pow(99)
+  def Trestrigintillion: BigInt = 10.pow(102)
+  def Quattuortrigintillion: BigInt = 10.pow(105)
+  def Quinquatrigintillion: BigInt = 10.pow(108)
+  def Sestrigintillion: BigInt = 10.pow(111)
+  def Septentrigintillion: BigInt = 10.pow(114)
+  def Octotrigintillion: BigInt = 10.pow(117)
+  def Noventrigintillion: BigInt = 10.pow(120)
+  def Quadragintillion: BigInt = 10.pow(123)
+  //
+  def Quinquagintillion: BigInt = 10.pow(153)
+  def Sexagintillion: BigInt = 10.pow(183)
+  def Septuagintillion: BigInt = 10.pow(213)
+  def Octogintillion: BigInt = 10.pow(243)
+  def Nonagintillion: BigInt = 10.pow(273)
+  //
+  def Centillion: BigInt = 10.pow(303)
+  def Uncentillion: BigInt = 10.pow(306)
+  def Duocentillion: BigInt = 10.pow(309)
+  def Trescentillion: BigInt = 10.pow(312)
+  //
+  def Decicentillion: BigInt = 10.pow(333)
+  def Undecicentillion: BigInt = 10.pow(336)
+  def Viginticentillion: BigInt = 10.pow(363)
+  def Unviginticentillion: BigInt = 10.pow(366)
+  def Trigintacentillion: BigInt = 10.pow(393)
+  def Quadragintacentillion: BigInt = 10.pow(423)
+  def Quinquagintacentillion: BigInt = 10.pow(453)
+  def Sexagintacentillion: BigInt = 10.pow(483)
+  def Septuagintacentillion: BigInt = 10.pow(513)
+  def Octogintacentillion: BigInt = 10.pow(543)
+  def Nonagintacentillion: BigInt = 10.pow(573)
+  def Ducentillion: BigInt = 10.pow(603)
+  def Trecentillion: BigInt = 10.pow(903)
+  def Quadringentillion: BigInt = 10.pow(1203)
+  def Quingentillion: BigInt = 10.pow(1503)
+  def Sescentillion: BigInt = 10.pow(1803)
+  def Septingentillion: BigInt = 10.pow(2103)
+  def Octingentillion: BigInt = 10.pow(2403)
+  def Nongentillion: BigInt = 10.pow(2703)
+  def Millinillion: BigInt = 10.pow(3003)
+
+}
