b901a8a0244f8cd938408d00ce0bf1269b3d0f59
qilab- <qilab@gmx.com>
2016-05-22 23:16:37 +0900

    names of large numbers in CN

---
+++ src/main/scala/jp/qilab/sushi/largenumbers/constants/CN.scala
@@ -0,0 +1,30 @@
+package jp.qilab.sushi.largenumbers.constants
+
+import jp.qilab.sushi.largenumbers.Implicits._
+
+object CN {
+
+  def 一: Int = 1
+  def 十: Int = 10
+  def 百: Int = 100
+  def 千: Int = 10.pow(3).toInt
+  def 万: Int = 10.pow(4).toInt
+  def 億: Int = 10.pow(8).toInt
+  def 兆: Long = 10.pow(12).toLong
+  def 京: Long = 10.pow(16).toLong
+  def 垓: BigInt = 10.pow(20)
+//  def 𥝱: BigInt = 10.pow(24)
+  def 秭: BigInt = 10.pow(24)
+  def 穣: BigInt = 10.pow(28)
+  def 溝: BigInt = 10.pow(32)
+  def 澗: BigInt = 10.pow(36)
+  def 正: BigInt = 10.pow(40)
+  def 載: BigInt = 10.pow(44)
+  def 極: BigInt = 10.pow(48)
+  def 恒河沙: BigInt = 10.pow(52)
+  def 阿僧祇: BigInt = 10.pow(56)
+  def 那由他: BigInt = 10.pow(60)
+  def 不可思議: BigInt = 10.pow(64)
+  def 無量大数: BigInt = 10.pow(68)
+
+}
